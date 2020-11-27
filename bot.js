43aef0c92cd15044e8b59a1d507ee47515dfbfeb
Alexander Spies <alexander.paul.spies@gmail.com>
2020-11-27 16:27:18 +0100

    Bot now returns frequency of ips in log file.

--- bot.js
+++ bot.js
@@ -1,8 +1,27 @@
+const fs = require('fs')
+const readline = require('readline')
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.help((ctx) => ctx.reply('Supported commands: /frequency'))
+bot.command('frequency', (ctx) => {
+  const rl = readline.createInterface({
+    input: fs.createReadStream('access.log')
+  })
+
+  const freq = {}
+  rl.on('line', (line) => {
+    const ip = line.split(' ')[0]
+    freq[ip] = (freq[ip] + 1) || 1
+  })
+
+  rl.on('close', () => {
+    const list = Object.entries(freq)
+      .sort((a,b) => b[1] - a[1])
+    ctx.reply(list.join("\n"))
+  })
+})
+
+
 bot.launch()
