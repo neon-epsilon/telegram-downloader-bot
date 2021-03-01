2f5879162def249279c586bf4f268b69e47515ae
Alexander Spies <alexander.paul.spies@gmail.com>
2021-03-01 23:18:18 +0100

    Make bot return info on files sent in message

--- bot.js
+++ bot.js
@@ -4,24 +4,32 @@ const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Supported commands: /frequency'))
-bot.command('frequency', (ctx) => {
-  const rl = readline.createInterface({
-    input: fs.createReadStream('access.log')
-  })
 
-  const freq = {}
-  rl.on('line', (line) => {
-    const ip = line.split(' ')[0]
-    freq[ip] = (freq[ip] + 1) || 1
-  })
+bot.on('message', ctx => {
+  const files = ctx.update.message.photo
 
-  rl.on('close', () => {
-    const list = Object.entries(freq)
-      .sort((a,b) => b[1] - a[1])
-    ctx.reply(list.join("\n"))
-  })
+  if(files) {
+    ctx.reply(files)
+  }
 })
+//bot.help((ctx) => ctx.reply('Supported commands: /frequency'))
+//bot.command('frequency', (ctx) => {
+//  const rl = readline.createInterface({
+//    input: fs.createReadStream('access.log')
+//  })
+//
+//  const freq = {}
+//  rl.on('line', (line) => {
+//    const ip = line.split(' ')[0]
+//    freq[ip] = (freq[ip] + 1) || 1
+//  })
+//
+//  rl.on('close', () => {
+//    const list = Object.entries(freq)
+//      .sort((a,b) => b[1] - a[1])
+//    ctx.reply(list.join("\n"))
+//  })
+//})
 
 
 bot.launch()
