88feb2cd7a24508753fab26d427686616fbbe74f
Alexander Spies <alexander.paul.spies@gmail.com>
2021-03-13 17:43:47 +0100

    Turn "on.('message', "-function into closure
    This encapsulates the global variable globalMessageNumber.

--- bot.js
+++ bot.js
@@ -36,41 +36,43 @@ async function downloadFile(reply, url, fileName) {
   }
 }
 
+function makeMessageHandler() {
+  var globalMessageNumber = 0
+  return (async ctx => {
+    const localMessageNumber = ++globalMessageNumber
+    const messageTime = new Date()
+
+    const data = ctx.update.message.photo || ctx.update.message.video
+
+    if(!data) {
+      return
+    }
+
+    const fileId = ctx.update.message.photo ?
+      data.slice(-1)[0].file_id :
+      data.file_id
+
+    // Files bigger than 20MB may not be downloaded (per bot API); this throws a
+    // TelegramError.
+    try {
+      url =  await ctx.telegram.getFileLink(fileId)
+    } catch (error) {
+      ctx.reply('Error while getting file link: ' + error)
+      return
+    }
+
+    const timestamp = messageTime.toISOString().replace(/:/g, '').split('.')[0]
+    const urlFileName = url.split('/').slice(-1)[0]
+    const [base, extension] = urlFileName.split('.')
+    const fileName = downloadDirectory + '/' + timestamp + '_' + String(localMessageNumber).padStart(4, '0') + '.' + extension
+
+    downloadFile(ctx.reply, url, fileName)
+  })
+}
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
-
-var globalMessageNumber = 0
-bot.on('message', async ctx => {
-  const localMessageNumber = ++globalMessageNumber
-  const messageTime = new Date()
-
-  const data = ctx.update.message.photo || ctx.update.message.video
-
-  if(!data) {
-    return
-  }
-
-  const fileId = ctx.update.message.photo ?
-    data.slice(-1)[0].file_id :
-    data.file_id
-
-  // Files bigger than 20MB may not be downloaded (per bot API); this throws a
-  // TelegramError.
-  try {
-    url =  await ctx.telegram.getFileLink(fileId)
-  } catch (error) {
-    ctx.reply('Error while getting file link: ' + error)
-    return
-  }
-
-  const timestamp = messageTime.toISOString().replace(/:/g, '').split('.')[0]
-  const urlFileName = url.split('/').slice(-1)[0]
-  const [base, extension] = urlFileName.split('.')
-  const fileName = downloadDirectory + '/' + timestamp + '_' + String(localMessageNumber).padStart(4, '0') + '.' + extension
-
-  downloadFile(ctx.reply, url, fileName)
-})
+bot.on('message', makeMessageHandler() )
 
 bot.launch()
